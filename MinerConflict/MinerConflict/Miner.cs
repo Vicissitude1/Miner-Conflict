43a58b0dda66958a60fa30871e0d1cf77af53032
d-mad <d-mad@LAPTOP-474QNU92>
2017-03-19 17:43:35 +0100

    Miner and minerBuilder

---
+++ MinerConflict/MinerConflict/Miner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerConflict
+{
+    class Miner : SpeedUpgrade
+    {
+        
+        public Animator animator;
+        public int health;
+        public Direction direction;
+
+
+    }
+}
---
+++ MinerConflict/MinerConflict/SpeedUpgrade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerConflict
+{
+    abstract class SpeedUpgrade
+    {
+        public static float speed;
+
+        public SpeedUpgrade()
+        {
+            speed = 4f;
+        }
+    }
+}
