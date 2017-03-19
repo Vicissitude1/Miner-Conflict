d39e9eafc836e97089d2ce2f262bf487c6786e7f
d-mad <d-mad@LAPTOP-474QNU92>
2017-03-19 19:01:09 +0100

    Animator.....mer stuff

--- MinerConflict/MinerConflict/Animator.cs
+++ MinerConflict/MinerConflict/Animator.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
 
 namespace MinerConflict
 {
     class Animator
     {
+        private SpriteRenderer spriteRendere;
+        private int currentIndex;
+        private float timeElapsed;
+        private float fps;
+        private Rectangle[] reactangles;
+        private string animationName;
+
+        public Dictionary<string, Animation> Animations { get; set; }
+
+        public Animator(GameObject gameObject) : base(gameObject)
+        {
+            fps = 5;
+            this.spriteRendere = (SpriteRenderer)gameObject.GetComponent("SpriteRendere");
+            Animations = new Dictionary<string, Animation>();
+        }
     }
 }
