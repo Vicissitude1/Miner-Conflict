5ce8346e97ed53fd8eb3de6aab3b0bf6e312c85a
Computer <Computer@COMPUTER-PC>
2017-03-20 11:54:31 +0100

    tegner mine

---
+++ MinerConflict/MinerConflict/Builders/GoldMineBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerConflict.Builders
+{
+    class GoldMineBuilder : IBuilder
+    {
+        private GameObject gameObject;
+
+
+        public void Buildpart(Vector2 position)
+        {
+            gameObject = new GameObject(position);
+            gameObject.AddComponent(new SpriteRenderer(gameObject, Globals.Graphics.MineBuildingTexture, 1f, 0.5f));
+            gameObject.AddComponent(new Mine(gameObject));
+        }
+
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+    }
+}
--- MinerConflict/MinerConflict/Builders/MinerBuilder.cs
+++ MinerConflict/MinerConflict/Builders/MinerBuilder.cs
@@ -7,20 +7,7 @@ using System.Threading.Tasks;
 
 namespace MinerConflict.Builders
 {
-    class MinerBuilder : IBuilder
-    {
-        private GameObject gameObject;
-
-        public void Buildpart(Vector2 position)
-        {
-            gameObject = new GameObject(new Vector2(200, 200));
-            gameObject.AddComponent(new SpriteRenderer(gameObject, Globals.Graphics.MineBuildingTexture, 1f, 0.5f));
-            gameObject.AddComponent(new Mine(gameObject));
-        }
-
-        public GameObject GetResult()
-        {
-            return gameObject;
-        }
+    class MinerBuilder
+    { 
     }
 }
