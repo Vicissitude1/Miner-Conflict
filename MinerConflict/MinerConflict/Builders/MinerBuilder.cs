6bb68e4724dc19bf437010198a6c1a2db005d2df
Mikkel Tobias Koss <fishyniden@gmail.com>
2017-03-21 13:18:59 +0100

    Can't touch mah base

--- MinerConflict/MinerConflict/Builders/MinerBuilder.cs
+++ MinerConflict/MinerConflict/Builders/MinerBuilder.cs
@@ -8,6 +8,19 @@ using System.Threading.Tasks;
 namespace MinerConflict.Builders
 {
     class MinerBuilder
-    { 
+    {
+        private GameObject gameObject;
+
+        public void Buildpart(Vector2 position)
+        {
+            gameObject = new GameObject(position);
+            gameObject.AddComponent(new SpriteRenderer(gameObject, Globals.Graphics.MinerUnitTexture, 1f, 0.6f));
+            gameObject.AddComponent(new Miner(50, 0));
+        }
+
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
     }
 }
