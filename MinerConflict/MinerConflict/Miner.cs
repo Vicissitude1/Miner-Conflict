ee8e77a4533ec593e5d794e16c627d81aea10166
Computer <Computer@COMPUTER-PC>
2017-03-21 11:24:53 +0100

    thread i miner

--- MinerConflict/MinerConflict/Miner.cs
+++ MinerConflict/MinerConflict/Miner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 
@@ -9,7 +10,9 @@ namespace MinerConflict
 {
     class Miner : SpeedUpgrade
     {
-        
+
+        private static Object thisLockMiner = new Object();
+        private string instance;
         public Animator animator;
         public int health;
         public DIRECTION direction;
@@ -20,13 +23,24 @@ namespace MinerConflict
         {
             this.health = health;
             this.collected = collected;
-            
+
+            if (instance == null)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(Move));
+                t.IsBackground = true;
+                t.Start(15);
+            }
 
         }
 
         public void Update()
         {
 
+        }
+
+        public void Move(object obj)
+        {
+            
         }
 
         
