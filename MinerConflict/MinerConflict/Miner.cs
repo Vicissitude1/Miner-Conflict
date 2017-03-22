72702b53b0855b20f52d33d9a5730e0cd29b79f0
Computer <Computer@COMPUTER-PC>
2017-03-22 13:14:56 +0100

    something

--- MinerConflict/MinerConflict/Miner.cs
+++ MinerConflict/MinerConflict/Miner.cs
@@ -21,24 +21,41 @@ namespace MinerConflict
         public int health;
         public DIRECTION direction;
         private int collected;
+        private GameObject assocMine;
+        private GameObject minerpos;
+    
 
         public Miner ( int health, int collected, GameObject gameObject):base(gameObject)
         {
             this.health = health;
             this.collected = collected;
+            
         }
 
         public void Update()
         {
-            if (collected <= 0)
+            if (assocMine == null)
             {
-                this.gameObject.transform.Translate(new Vector2());
+                foreach (GameObject obj in GameWorld.Instance.GameObjects)
+                {
+                    if (obj.GetComponent("GoldMine") is GoldMine)
+                    {
+                        assocMine = obj;
+                        break;
+                    }
+                }
+            }
 
-                gameObject.transform.Translate(new Vector2(0, -10 * GameWorld.Instance.deltaTime));
+            if (minerpos.transform.Position.X >= assocMine.transform.Position.X || minerpos.transform.Position.Y >= assocMine.transform.Position.Y)
+            {
+
+                //gameObject.transform.Position.X += 0;
+                //gameObject.transform.Position.Y += -10;
+                gameObject.transform.Translate(new Vector2(0, +10 * GameWorld.Instance.deltaTime));
             }
             else if (collected > 0)
             {
-                gameObject.transform.Translate(new Vector2(0, +10 * GameWorld.Instance.deltaTime));
+                gameObject.transform.Translate(new Vector2(0, -10 * GameWorld.Instance.deltaTime));
 
             }
             else
